fce7f94a525929238c4f0ea7847c7003d7bef2ce
Thamjith <jithuthaha8@gmail.com>
2020-12-05 17:57:40 +0530

    React Frontend

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import logo from './logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="spaceX" style={{ width:300, display: 'block', margin: 'auto' }}></img>
+    </div>
+  );
+}
+
+export default App;
