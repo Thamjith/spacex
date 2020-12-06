e4b6afc69256fcf09f885459af8ec312532d903f
Thamjith <jithuthaha8@gmail.com>
2020-12-06 16:28:49 +0530

    Launch Details By id

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import logo from './logo.png';
 import Launches from './components/Launches'
+import Launch from './components/Launch'
 
 function App() {
   return (
+    <Router>
       <div className="container">
         <img src={logo} alt="spaceX" style={{ width:300, display: 'block', margin: 'auto' }}></img>
-        <Launches />
+        <Route exact path="/" component={Launches} />
+        <Route exact path="/launch/:flight_number" component={Launch} />
       </div>
+    </Router>
   );
 }
 
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useQuery, gql } from '@apollo/client';
 import LaunchItem from './LaunchItem'
+import MissionKey from './MissionKey'
 
 const LAUNCHES_RATES = gql`
   query LaunchesQuery {
@@ -19,7 +20,7 @@ const Launches = () => {
 
     return(
         <>
-            { console.log('data = ', data) }
+            <MissionKey />
             { loading ? "Loading" : error ? "ERROR" : 
                 data.launches.map((launch) => (
                     <LaunchItem key={launch.flight_number} launch={launch} />
@@ -30,19 +31,3 @@ const Launches = () => {
 }
 
 export default Launches
-
-
-// const Launches = () => {
-//     const { loading, error, data } = useQuery(LAUNCHES_RATES);
-
-//     if (loading) return <p>Loading...</p>;
-//     if (error) return console.log(error);
-
-//     return data.launches.map(({ flight_number, mission_name, launch_date_local, launch_success }) => (
-//         <div key={flight_number}>
-//             <p>
-//                 { mission_name }
-//             </p>
-//         </div>
-//     ));
-// }
